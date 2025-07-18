f861f7c1b60cc9aa39832ce8b5e77b41e5feeddd
kurachi <kurachi@nces.i.nagoya-u.ac.jp>
2025-07-18 16:29:44 +0900

    Update

--- icsim.c
+++ icsim.c
@@ -133,6 +133,9 @@ void update_doors(CarState* state) {
   SDL_Rect dst_white = {src_white.x - offset.x, src_white.y - offset.y, src_white.w, src_white.h};
   SDL_RenderCopy(renderer, sprite_tex, &src_white, &dst_white);
 
+  for (int i = 0; i < 4; ++i) {
+    printf("door_status[%d] = %d\n", i, state->door_status[i]);
+  }
   // Red body is drawn if any door is unlocked
   for (int i = 0; i < 4; ++i) {
     if (state->door_status[i] == DOOR_UNLOCKED) {
@@ -279,6 +282,9 @@ void update_door_status(struct canfd_frame *cf, int maxdlen) {
 	car_state.door_status[3] = DOOR_LOCKED;
   } else {
 	car_state.door_status[3] = DOOR_UNLOCKED;
+  }
+  for (int i = 0; i < 4; ++i) { // Debug output for door status
+    printf("door_status[%d] = %d\n", i, car_state.door_status[i]);
   }
 }
 
